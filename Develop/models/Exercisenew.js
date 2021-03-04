932efea0c70f774f1a90bc576ae569873ee7ae93
Maria Ortega <mariaortegapb@gmail.com>
2021-03-04 07:51:18 -0500

    fixed exercisenew Schema

--- Develop/models/Exercisenew.js
+++ Develop/models/Exercisenew.js
@@ -1,20 +1,19 @@
 const mongoose = require("mongoose"); 
 const Schema = mongoose.Schema; 
 
-const newExerciseSchema = new Schema({ 
+const ExercisenewSchema = new Schema({ 
 
 name: { 
 type: String, 
 unique: true 
 }, 
 exercises: [ 
-{ 
-
+    {  
 type: Schema.Types.ObjectId, 
 ref: "Exercise" 
-} 
+    } 
 ] 
 }); 
 
-const Library = mongoose.model("newExercises", LibrarySchema); 
-module.exports = Library; 
+const Library = mongoose.model("Exercisenew", ExercisenewSchema); 
+module.exports = Exercisenew; 
